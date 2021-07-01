96f4470fc94e02a4178c6d9b3ff2ca29fe3d627f
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2021-07-01 16:23:24 -0300

    Update multi_activation.py
    Adding an useful stuff to apply the correct torch activation functions. Also, softmax2d and prelu needs additional correction to work

--- torch/multi_activation.py
+++ torch/multi_activation.py
@@ -36,12 +36,45 @@ class MultiActivation(Module):
     def __init__(self, activation: Tuple[str, ...], strategy: str = 'mean') -> None:
         super(MultiActivation, self).__init__()
 
+	# Softmax2d, PReLU is not working yet
+
         valid_activation_functions = [
-            'Threshold', 'ReLU', 'Hardtanh', 'ReLU6', 'Sigmoid', 'Tanh', 'Softmax',
-            'Softmax2d', 'LogSoftmax', 'ELU', 'SELU', 'CELU', 'GLU', 'GELU', 'Hardshrink',
-            'LeakyReLU', 'LogSigmoid', 'Softplus', 'Softshrink', 'PReLU', 'Softsign', 'Softmin',
-            'Tanhshrink', 'RReLU', 'Hardsigmoid', 'Hardswish', 'SiLU'
+            'Threshold()', 'ReLU()', 'Hardtanh()', 'ReLU6()', 'Sigmoid()', 'Tanh()', 'Softmax()',
+            'Softmax2d()', 'LogSoftmax()', 'ELU()', 'SELU()', 'CELU()', 'GELU()', 'Hardshrink()',
+            'LeakyReLU()', 'LogSigmoid()', 'Softplus()', 'Softshrink()', 'PReLU()', 'Softsign()', 'Softmin()',
+            'Tanhshrink()', 'RReLU()', 'Hardsigmoid()', 'Hardswish()', 'SiLU()','Mish()'
         ]
+
+        valid_torch_functions = {
+            'Threshold()': torch.nn.Threshold(threshold=5, value=0),
+            'ReLU()': torch.nn.ReLU(),
+            'Hardtanh()': torch.nn.Hardtanh(),
+            'ReLU6()': torch.nn.ReLU6(),
+            'Sigmoid()': torch.nn.Sigmoid(),
+            'Tanh()': torch.nn.Tanh(),
+            'Softmax()': torch.nn.Softmax(),
+            'Softmax2d()': torch.nn.Softmax2d(),
+            'LogSoftmax()': torch.nn.LogSoftmax(),
+            'ELU()': torch.nn.ELU(),
+            'SELU()': torch.nn.SELU(), 
+            'CELU()': torch.nn.CELU(),
+            'GELU()': torch.nn.GELU(), 
+            'Hardshrink()': torch.nn.Hardshrink(),
+            'LeakyReLU()': torch.nn.LeakyReLU(), 
+            'LogSigmoid()': torch.nn.LogSigmoid(), 
+            'Softplus()': torch.nn.Softplus(), 
+            'Softshrink()': torch.nn.Softshrink(), 
+            'PReLU()': torch.nn.PReLU(), 
+            'Softsign()': torch.nn.Softsign(), 
+            'Softmin()': torch.nn.Softmin(),
+            'Tanhshrink()': torch.nn.Tanhshrink(), 
+            'RReLU()': torch.nn.RReLU(), 
+            'Hardsigmoid()': torch.nn.Hardsigmoid(), 
+            'Hardswish()': torch.nn.Hardswish(), 
+            'SiLU()': torch.nn.SiLU(),
+            'Mish()': torch.nn.Mish()
+        }
+
         if not isinstance(activation, tuple):
             raise TypeError(
                 'Invalid activation type {!r}, should be tuple'.format(
@@ -50,13 +83,15 @@ class MultiActivation(Module):
             )
         self.activation = ()
         for a in activation:
-            if type(a).__name__ not in valid_activation_functions:
+            if a not in valid_activation_functions:
+            #if type(a).__name__ not in valid_activation_functions:
                 raise ValueError(
                     'Invalid activation function {!r}, should be one of {}'.format(
                         a, valid_activation_functions
                     )
                 )
-            self.activation += (a,)
+            self.activation += (valid_torch_functions[a],)
+        print(self.activation)
 
         valid_strategy_strings = {'concat', 'mean'}
         if not isinstance(strategy, str):
@@ -76,8 +111,8 @@ class MultiActivation(Module):
     def _multi_activation_forward(self, input: Tensor) -> Tensor:
         if self.strategy == 'concat':
             return torch.hstack([a(input) for a in self.activation])
-
-        return torch.mean(torch.stack([a(input) for a in self.activation]), dim=0)
+        else:
+            return torch.mean(torch.stack([a(input) for a in self.activation]), dim=0)
 
     def forward(self, input: Tensor) -> Tensor:
         return self._multi_activation_forward(input)
