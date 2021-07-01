d19351d1a80678cf693cc59410b0eaa952f2e93e
Mateus Roder <45532871+MateusRoder@users.noreply.github.com>
2021-07-01 16:47:03 -0300

    adding files

---
+++ convnets/convnet.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from framework.torch.multi_activation import MultiActivation
+
+class Net(nn.Module):
+    def __init__(self, chn=3, n_filters=(32, 32), kernel_size=(5, 5), p=(0.25,)):
+
+        super(Net, self).__init__()
+	# Softmax2d, PReLU are not working yet
+        self.conv1 = nn.Conv2d(chn, n_filters[0], kernel_size[0])
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(n_filters[0], n_filters[1], kernel_size[1])
+        self.dropout1 = nn.Dropout2d(p[0])
+        self.fc1 = nn.Linear(n_filters[1]*kernel_size[1]*kernel_size[1], 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        self.multi = MultiActivation(activation=('ReLU()', 'ReLU6()', 'SiLU()', 'Mish()',), strategy='mean')
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        #x = F.relu(self.fc1(x))
+        #x = F.relu(self.fc2(x))
+        x = self.multi(self.fc1(x))
+        x = self.multi(self.fc2(x))
+        x = self.fc3(x)
+        return x
